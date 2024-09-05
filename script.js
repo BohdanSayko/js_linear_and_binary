f3fb993e43f5b30051dcff46cd4c37674709efc0
unknown <b.saicko2020@ukr.net>
2024-09-05 20:24:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function binary(arr, findNum) {
+    let left = 0;
+    let right = arr.length - 1;
+  
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+  
+      if (arr[mid] == findNum) {
+        return mid; 
+      } else if (arr[mid] < findNum) {
+        left = mid; 
+      } else {
+        right = mid; 
+      }
+    }  
+    return -1; 
+}
+
+function linear(arr, findNum) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == findNum) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+let arr = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+    51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+    71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+    81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+    91, 92, 93, 94, 95, 96, 97, 98, 99, 100
+];
+
+let findNum = 87;
+
+let resultLinear = linear(arr, findNum);
+let resultBinary = binary(arr, findNum);
+
+if (resultBinary !== -1) {
+    console.log("Binary: ", arr[resultBinary]);
+} else {
+    console.log("Binary: Number not found");
+}
+
+if (resultLinear !== -1) {
+    console.log("Linear: ", arr[resultLinear]);
+} else {
+    console.log("Linear: Number not found");
+}
